08eaa0f99941233be6863265d296b7e43818e242
AntonLED <al3406099@gmail.com>
2024-11-06 12:33:29 +0300

    asdc

--- src/docs/source/conf.py
+++ src/docs/source/conf.py
@@ -60,7 +60,7 @@ breathe_default_project = "Dephan ROS driver"
 # -- Options for HTML output -------------------------------------------------
 html_context = {
   'current_version': 'ROS:iron',
-  'versions' : [["ROS:noetic", "https://antonled.github.io/noetic"], ["ROS:iron", "https://antonled.github.io/iron"]],
+  'versions' : [["ROS:noetic", "https://antonled.github.io/_test/noetic"], ["ROS:iron", "https://antonled.github.io/_test/iron"]],
 }
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
